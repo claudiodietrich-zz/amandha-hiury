06f1c26c58334724a033c56d0f7b486d8e96f816
Claudio Dietrich <crdietrichjr@gmail.com>
2019-06-08 16:12:28 -0300

    add: countdown

--- client/src/components/Hero.vue
+++ client/src/components/Hero.vue
@@ -1,19 +1,52 @@
 <template>
   <section class="hero has-text-centered parallax">
-    <div class="hero-body">
-      <div class="container">
-        <h1 class="title">
-          Amandha e Hiury
-        </h1>
-        <h2 class="subtitle">
-          Hero subtitle
-        </h2>
+    <div class="parallax-curtain">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">
+            Amandha e Hiury
+          </h1>
+          <h2 class="subtitle">
+            {{ countdownTimer }}
+          </h2>
+        </div>
       </div>
     </div>
   </section>
 </template>
 
+<script>
+import countdown from 'countdown'
+
+countdown.setLabels(' milissegundo| seg| min| h| dia| semana| mês| ano| década| século| milênio', ' milissegundos| seg| min| h| dias| semanas| meses| anos| décadas| séculos| milênios', ' e ', ', ', 'agora')
+
+export default {
+  data () {
+    return {
+      countdownTimer: ''
+    }
+  },
+  created () {
+    this.countdownTimer = countdown(new Date('July 20 2019 19:00')).toString()
+
+    setInterval(() => {
+      this.countdownTimer = countdown(new Date('July 20 2019 19:00')).toString()
+    }, 1000)
+  }
+}
+</script>
+
 <style scoped>
+.title {
+  font-size: 5rem !important;
+  color: white;
+}
+
+.subtitle {
+  font-size: 1.5rem !important;
+  color: white
+}
+
 .parallax {
   background-attachment: fixed;
   background-position: center;
@@ -22,5 +55,10 @@
   height: 100vh;
   width: 100vw;
   background-image: url('../assets/hero/background.jpg');
+}
+
+.parallax-curtain{
+  height:100vh;
+  background:rgba(0, 0, 0, 0.5);
 }
 </style>
